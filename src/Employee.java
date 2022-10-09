947f496945831828b001d24abcff5c819b11a87a
blade0220 <blade0220@yandex.ru>
2022-10-09 16:15:52 +0300

    Проверьте мою курсовую

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class Employee {
+public class Employee {
+    private int salary;
+    private int department;
+    private final String fullName;
+    private final int id;
+    private static int counter = 0;
+
+    public Employee (String fullName, int department, int salary){
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+        this.id = counter++;
+
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary){
+        this.salary = salary;
+    }
+
+    public int getDepartment(){
+        return department;
+    }
+
+    public void setDepartment(int department){
+        this.department = department;
+    }
+
+
+
+    public String getFullName(){
+        return this.fullName;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Сотрудника {" +
+                "Зарплата = " + salary +
+                ", Отдел №" + department +
+                ", ФИО: " + fullName +
+                ", id=" + id +
+                '}';
+    }
+
+
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,82 @@
+import java.sql.Array;
+import java.util.Arrays;
+
 public class Main {
+
+    private static final Employee[] employees = new Employee[10];
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        employees[0] = new Employee("Иванова Анна Викторовна", 1,70_000);
+        employees[1] = new Employee("Семёнов Алексей Анатольевич", 1,50_000);
+        employees[2] = new Employee("Аксенов Семён Николаевич", 2, 90_000);
+        employees[3] = new Employee("Алексеева Татьяна Михайловна", 2, 85_000);
+        employees[4] = new Employee("Зорина Любовь Станиславовна", 3, 75_000);
+        employees[5] = new Employee("Васильева Мария Васильевна", 3, 80_000);
+        employees[6] = new Employee("Петров Игорь Алексеевич", 4, 95_000);
+        employees[7] = new Employee("Любимов Василий Григорьевич", 4, 80_000);
+        employees[8] = new Employee("Михайлова Анастасия Генадьевна", 5, 55_000);
+        employees[9] = new Employee("Павлов Георгий Афанасьевич", 5, 65_000);
+        System.out.println("Итого израсходовано на зарплату сотрудников " + calculateSumSalary());
+
+        System.out.println("Максимальная зарплата " + getEmployeeWithMaxSalary());
+
+        System.out.println("Минимальная зарплата " + getEmployeeWithMinSalary());
+        System.out.println("Средняя зарплата равна: " + middleSumSalary());
+        printEmployeesName();
+
+
+    }
+
+    public static int calculateSumSalary() {
+        int sum = 0;
+        for (Employee employee : employees) {
+            if (employee != null) {
+                sum += employee.getSalary();
+            }
+        }
+        return sum;
+    }
+
+    public static int middleSumSalary() {
+        int sum = 0;
+        for (Employee employee : employees) {
+            if (employee != null) {
+                sum += employee.getSalary() / employees.length;
+            }
+        }
+        return sum;
+    }
+
+    public static void printEmployeesName(){
+       for (Employee employee:employees) {
+           if (employee != null) {
+               System.out.println(employee.getFullName());
+
+           }
+       }
+    }
+
+    public static Employee getEmployeeWithMaxSalary(){
+        int max = Integer.MIN_VALUE;
+        Employee targetEmployee = null;
+        for (Employee employee : employees){
+            if (employee != null && employee.getSalary() > max){
+                max = employee.getSalary();
+                targetEmployee = employee;
+            }
+        }
+        return targetEmployee;
+    }
+
+    public static Employee getEmployeeWithMinSalary(){
+        int min = Integer.MAX_VALUE;
+        Employee targetEmployee = null;
+        for (Employee employee : employees){
+            if (employee != null && employee.getSalary() < min){
+                min = employee.getSalary();
+                targetEmployee = employee;
+            }
+        }
+        return targetEmployee;
     }
 }
