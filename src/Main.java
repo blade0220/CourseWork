f2812d48c95988be2a65aa8c007b1b768bb0c729
blade0220 <blade0220@yandex.ru>
2022-10-15 23:38:45 +0300

    Курсовая. Доработка.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Main {
 
     private static final Employee[] employees = new Employee [10];
@@ -16,9 +14,8 @@ public class Main {
         employees[8] = new Employee("Михайлова Анастасия Генадьевна", 5, 55_000);
         employees[9] = new Employee("Павлов Георгий Афанасьевич", 5, 65_000);
 
-        for (int i = 0; i < employees.length; i++)
-        System.out.println(employees[i].toString());
 
+        employees();
         System.out.println("Итого израсходовано на зарплату сотрудников " + calculateSumSalary());
 
         System.out.println("Максимальная зарплата " + getEmployeeWithMaxSalary());
@@ -27,6 +24,14 @@ public class Main {
         System.out.println("Средняя зарплата равна: " + middleSumSalary());
         printEmployeesName();
 
+    }
+
+    private static void employees(){
+        for (int i = 0; i < employees.length; i++){
+            if (employees[i] != null) {
+                System.out.println(employees[i].toString());
+            }
+        }
     }
 
     public static int calculateSumSalary() {
@@ -38,14 +43,6 @@ public class Main {
         }
         return sum;
     }
-
-
-
-//    for (int i = 0; i < employees.length; i++){
-//        if (employees[i] != null){
-//            System.out.println(employees[i]);
-//        }
-//    }
 
     public static double middleSumSalary() {
         double sum = 0;
